d8567ac2e5af60d2c8d44fd3c0bfd1399244a836
Sridhar Arjun <sridharmuraleedar@gmail.com>
2021-04-16 01:39:18 +0530

    only animate the list icons when in view

--- src/components/LandingComponent/landing.js
+++ src/components/LandingComponent/landing.js
@@ -3,20 +3,27 @@ import CreaLogo from "./crea-logo.png"
 import { useEffect } from "react";
 
 function Landing(){
+    
     useEffect(()=>{
         var delayInMilliseconds = 3000;
         setTimeout(function(){
-            var focus = document.getElementById("connect-label");
-            focus.scrollIntoView({
+            var animatedIconsList = document.getElementById("icons").getElementsByTagName("li");
+            var reachLabel = document.getElementById("connect-label");
+            reachLabel.scrollIntoView({
                 behavior:"smooth"
             });
+            reachLabel.classList.add("animateNow")
+            for (var i = 0; i < animatedIconsList.length; i++) {
+                animatedIconsList[i].classList.add("animateNow");
+            }
         },delayInMilliseconds);
     });
+
     return(<div class="container">
     <div class="item-1">
         <img id="logo" src={CreaLogo} alt="CREA Logo" />
     </div>
-    <div id="connect-label" class="connect-label">Connect with us through</div>
+    <div id="connect-label">Connect with us through</div>
     <div class="item-2">
         <ul id="icons" class="social-icons">
             <li class="website">
