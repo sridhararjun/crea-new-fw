df805e7e934bd449d9127d7ec3b5c2e619fbf2a9
dv2 <dv2@jcp.com>
2021-07-07 22:07:28 +0530

    buttonComponent Integration

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,11 +3,11 @@ import React from "react";
 import "./Button.css";
 
 export default function Button (props) {
-    const {iconName, btnName} = props;
+    const {iconName, btnName, hrefValue} = props;
     return(
-        <li className="listWrapper">
+        <li className="listWrapper social-iconss">
             <p className="outerborder"></p>
-        <a href="mailto:crealuxurymetaldecor@gmail.com">
+        <a href={hrefValue}>
            <i className={iconName} ></i>
             <div className="btnContent">
                 <p>{btnName}</p>
